aebe6ebe3785bdec63bdd235d0751f6dab726c17
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-05-14 18:24:24 +0100

    Adds testing with randomly generated data

--- bin/load_db_scxa_analytics.sh
+++ bin/load_db_scxa_analytics.sh
@@ -14,8 +14,21 @@ set -e
 
 # TODO this type of function should be loaded from a common set of scripts.
 checkExecutableInPath() {
-  [[ ! $(type -P $1) ]] && echo "$1 binaries not in the path." && exit 1
-  [[ ! -x `type -P $1` ]] && echo "$1 is not executable." && exit 1
+  [[ ! $(type -P $1) ]] && echo "$1 binaries not in the path." && exit 1;
+  [[ ! -x `type -P $1` ]] && echo "$1 is not executable." && exit 1;
+}
+
+checkDatabaseConnection() {
+  pg_user=`echo $1 | sed s+postgresql://++ | awk -F':' '{ print $1}'`
+  pg_host_port=`echo $1 | awk -F':' '{ print $3}' \
+           | awk -F'@' '{ print $2}' | awk -F'/' '{ print $1 }'`
+  pg_host=`echo $pg_host_port  | awk -F':' '{print $1}'`
+  pg_port=`echo $pg_host_port  | awk -F':' '{print $2}'`
+  if [ ! -z "$pg_port" ]; then
+    pg_isready -U $pg_user -h $pg_host -p $pg_port || (echo "No db connection." && exit 1)
+  else
+    pg_isready -U $pg_user -h $pg_host || (echo "No db connection" && exit 1)
+  fi
 }
 
 
@@ -35,7 +48,7 @@ ATLAS_SC_EXPERIMENTS=${ATLAS_SC_EXPERIMENTS:-$3}
 matrix_path=$ATLAS_SC_EXPERIMENTS/$EXP_ID".expression_tpm.mtx.gz"
 genes_path=$ATLAS_SC_EXPERIMENTS/$EXP_ID".expression_tpm.mtx_rows.gz"
 runs_path=$ATLAS_SC_EXPERIMENTS/$EXP_ID".expression_tpm.mtx_cols.gz"
-for f in $matrix_path, $genes_path, $runs_path; do
+for f in $matrix_path $genes_path $runs_path; do
   [ ! -e $f ] && echo "$EXP_ID: Matrix file $f missing, exiting." && exit 1
 done
 
@@ -45,39 +58,42 @@ for e in pg_isready, sed, psql, matrixMarket2csv.R; do
 done
 
 # Check that database connection is valid
-pg_isready $dbConnection
+checkDatabaseConnection $dbConnection
 
 # Transform experiment accession so that it can be used as a table name.
-lc_exp_acc=`echo $EXP_ID | lc | sed 's/-/_/g'`
+lc_exp_acc=`echo $EXP_ID | tr '[:upper:]' '[:lower:]' | sed 's/-/_/g'`
 # Delete partition for experiment if already exists
-sed s/<EXP-ACCESSION>/$lc_exp_acc/ postgres_scripts_dir/01-delete_existing_partition.sql.template | \
+sed "s/<EXP-ACCESSION>/$lc_exp_acc/" $postgres_scripts_dir/01-delete_existing_partition.sql.template | \
 psql $dbConnection
 # Create partition table.
-sed s/<EXP-ACCESSION>/$lc_exp_acc/ postgres_scripts_dir/02-create_partition_table.sql.template | \
+sed "s/<EXP-ACCESSION>/$lc_exp_acc/" $postgres_scripts_dir/02-create_partition_table.sql.template | \
 psql $dbConnection
-
 # Create file with data
-matrixMarket2csv.R -m $matrix_path -g $genes_path -r $runs_path -s 10000 -o $ATLAS_SC_EXPERIMENTS/expression2load.csv
+matrixMarket2csv.R -m $matrix_path -r $genes_path -c $runs_path \
+                   -s 10000 -o $ATLAS_SC_EXPERIMENTS/expression2load.csv \
+                   -e $EXP_ID
 
 # Load data into partition table
-sed s/<EXP-ACCESSION>/$lc_exp_acc/ postgres_scripts_dir/03-load_data.sql.template | \
-    sed s+<PATH-TO-DATA>+$ATLAS_SC_EXPERIMENTS/expression2load.csv+ | \
+sed "s/<EXP-ACCESSION>/$lc_exp_acc/" $postgres_scripts_dir/03-load_data.sql.template | \
+    sed "s+<PATH-TO-DATA>+$ATLAS_SC_EXPERIMENTS/expression2load.csv+" | \
     psql $dbConnection
 
-rm $ATLAS_SC_EXPERIMENTS/expression2load.csv
+#rm $ATLAS_SC_EXPERIMENTS/expression2load.csv
 
 # Create primary key.
-sed s/<EXP-ACCESSION>/$lc_exp_acc/g postgres_scripts_dir/04-build_pk.sql.template | \
+sed "s/<EXP-ACCESSION>/$lc_exp_acc/g" $postgres_scripts_dir/04-build_pk.sql.template | \
     psql $dbConnection
 
 # Post-process partition table
-sed s/<EXP-ACCESSION>/$lc_exp_acc/g postgres_scripts_dir/05-post_processing.sql.template | \
-    sed s/<EXP-ACC-UC>/$EXP_ID/g | \
+sed "s/<EXP-ACCESSION>/$lc_exp_acc/g" $postgres_scripts_dir/05-post_processing.sql.template | \
+    sed "s/<EXP-ACC-UC>/$EXP_ID/g" | \
     psql $dbConnection
 
+echo "Post-processing done"
+
 # Attach partition
-sed s/<EXP-ACCESSION>/$lc_exp_acc/g postgres_scripts_dir/06-attach_partition.sql.template | \
-    sed s/<EXP-ACC-UC>/$EXP_ID/g | \
+sed "s/<EXP-ACCESSION>/$lc_exp_acc/g" $postgres_scripts_dir/06-attach_partition.sql.template | \
+    sed "s/<EXP-ACC-UC>/$EXP_ID/g" | \
     psql $dbConnection
 
 echo "Partition table loaded for experiment $EXP_ID succesfully."
