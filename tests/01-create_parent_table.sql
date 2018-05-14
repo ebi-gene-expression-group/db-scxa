12e21eb4a2fa97db4daf1edd25df3be7a640915a
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-05-14 18:28:54 +0100

    Adds creation of base table for testing.

---
+++ tests/01-create_parent_table.sql
@@ -0,0 +1,17 @@
+/*
+  This script creates the master table where partitions will
+  be plugged in
+  No indices or other constraints can be defined at this level
+*/
+
+--DROP TABLE IF EXISTS scxa_analytics CASCADE; 
+
+CREATE TABLE IF NOT EXISTS scxa_analytics
+(
+  experiment_accession VARCHAR(255)     NOT NULL,
+  gene_id              VARCHAR(255)     NOT NULL,
+  cell_id              VARCHAR(255)     NOT NULL,
+  expression_level     DOUBLE PRECISION
+)
+  PARTITION BY LIST (experiment_accession)
+;
