a5e306dc5eeda7c20a72b17f80755481f5da9b94
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-05-23 14:37:46 +0100

    Adds loading and testing for marker genes

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-testsDir=$scriptDir
+export testsDir=$scriptDir
 export PATH=$scriptDir/../bin:$scriptDir/../tests:$PATH
 
+# For analytics loading testing
 psql $dbConnection < $testsDir/01-create_parent_table.sql
 
 create-test-matrix-market-files.R TEST-EXP1
@@ -11,6 +12,9 @@ create-test-matrix-market-files.R TEST-EXP2
 
 export EXPERIMENT_MATRICES_PATH=$PWD
 
+# For marker genes loading testing
+psql $dbConnection < $testsDir/marker-genes/01-optional-create-table.sql
+export EXPERIMENT_MGENES_PATH=$testsDir/marker-genes
 
 if [ "$#" -eq 0 ]; then
 	bats --tap "$(dirname "${BASH_SOURCE[0]}")"
