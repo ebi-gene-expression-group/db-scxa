08b428776ea22d9d22b203eba49f6109cfa5271f
Pablo Moreno <pcm32@users.noreply.github.com>
2019-08-30 13:09:10 +0100

    Update reindex to new name of scxa_experiment

--- bin/reindex_tables.sh
+++ bin/reindex_tables.sh
@@ -5,7 +5,7 @@ REINDEX TABLE scxa_tsne;
 REINDEX TABLE scxa_marker_genes;
 REINDEX TABLE scxa_cell_clusters;
 REINDEX TABLE scxa_analytics;
-REINDEX TABLE scxa_experiment;
+REINDEX TABLE experiment;
 
 CLUSTER;
 EOF
