442ae2d2d881f3b8b91d3f11801493cd3b652302
Pablo Moreno <pablo.a.moreno@gmail.com>
2020-04-02 18:35:44 +0100

    Logic for tests with containers on single bash script

---
+++ run_tests_with_containers.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Above ^^ will test that bash is installed
+
+dbConnection='postgresql://scxa:postgresPass@postgres/scxa-test'
+docker stop postgres && docker rm postgres
+docker network rm mynet
+docker network create mynet
+docker run --name postgres --net mynet -e POSTGRES_PASSWORD=postgresPass -e POSTGRES_USER=scxa -e POSTGRES_DB=scxa-test -p 5432:5432 -d postgres:10.3-alpine
+docker build -t test/db-scxa-module .
+docker run --net mynet -i -v $( pwd )/tests:/usr/local/tests -e dbConnection=$dbConnection -v $( pwd )/atlas-schemas:/atlas-schemas --entrypoint=/usr/local/tests/run_tests.sh test/db-scxa-module
--- tests/run_tests_inside_container.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Above ^^ will test that bash is installed
-
-apk add bats
-
-/usr/local/tests/run_tests.sh
