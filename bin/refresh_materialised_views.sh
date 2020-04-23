f31630aef4690bd8963fa6783a2412bd0a594e6b
Pablo Moreno <pablo.a.moreno@gmail.com>
2020-04-23 16:02:31 +0100

    Use more memory for expensive wrap up operations.

--- bin/refresh_materialised_views.sh
+++ bin/refresh_materialised_views.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -e
-echo "REFRESH MATERIALIZED VIEW scxa_top_5_marker_genes_per_cluster;" | psql -v ON_ERROR_STOP=1 $dbConnection
-echo "REFRESH MATERIALIZED VIEW scxa_marker_gene_stats;" | psql -v ON_ERROR_STOP=1 $dbConnection
+
+psql -v ON_ERROR_STOP=1 $dbConnection <<EOF
+SET maintenance_work_mem='2GB';
+REFRESH MATERIALIZED VIEW scxa_top_5_marker_genes_per_cluster;
+REFRESH MATERIALIZED VIEW scxa_marker_gene_stats;
+RESET maintenance_work_mem;
+EOF
