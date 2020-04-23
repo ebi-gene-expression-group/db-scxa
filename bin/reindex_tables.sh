d0e23d4e6b94411898c5a4a0c337131e727b99af
Pablo Moreno <pcm32@users.noreply.github.com>
2020-04-23 16:35:59 +0100

    Use more memory for expensive wrap up operations. (#38)

--- bin/reindex_tables.sh
+++ bin/reindex_tables.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -e
 psql -v ON_ERROR_STOP=1 $dbConnection <<EOF
+SET maintenance_work_mem='2GB';
 REINDEX TABLE scxa_tsne;
 REINDEX TABLE scxa_marker_genes;
 REINDEX TABLE scxa_cell_clusters;
 REINDEX TABLE experiment;
 
 CLUSTER;
+RESET maintenance_work_mem;
 EOF
