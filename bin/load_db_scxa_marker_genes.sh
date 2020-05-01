02da3e7279b0cbf44183fe6b75960415f478fd81
Jonathan Manning <jmanning@ebi.ac.uk>
2020-05-01 15:05:17 +0100

    Add candidate code for loading markers into normalised table structure

--- bin/load_db_scxa_marker_genes.sh
+++ bin/load_db_scxa_marker_genes.sh
@@ -31,6 +31,12 @@ fi
 # Check that database connection is valid
 checkDatabaseConnection $dbConnection
 
+markerGenesToLoad=$markerGenesToLoad
+groupIds=$SCRATCH_DIR/groupIds.csv
+inferredCelltypeMarkers=$EXPERIMENT_MGENES_PATH/celltype_markers.tsv
+authorsInferredCelltypeMarkers=$EXPERIMENT_MGENES_PATH/authors_celltype_markers.tsv
+
+
 if [[ -z ${NUMBER_MGENES_FILES+x} || $NUMBER_MGENES_FILES -gt 0 ]]; then
   # Check that files are in place.
   [ $(ls -1 $EXPERIMENT_MGENES_PATH/$MGENES_PREFIX*$MGENES_SUFFIX | wc -l) -gt 0 ] \
@@ -50,7 +56,7 @@ if [[ -z ${NUMBER_MGENES_FILES+x} || $NUMBER_MGENES_FILES -gt 0 ]]; then
   # - Column ordering on the marker genes file: clusts padj auroc feat
   # - Table ordering of columns: experiment_accession gene_id k cluster_id marker_probability
   echo "Marker genes: Create data file for $EXP_ID..."
-  rm -f $EXPERIMENT_MGENES_PATH/mgenesDataToLoad.csv
+  rm -f $markerGenesToLoad
   for f in $(ls $EXPERIMENT_MGENES_PATH/$MGENES_PREFIX*$MGENES_SUFFIX); do
     k=$(echo $f | sed s+$EXPERIMENT_MGENES_PATH/$MGENES_PREFIX++ | sed s/$MGENES_SUFFIX// )
     if [ -e $EXPERIMENT_MGENES_PATH/$EXP_ID.clusters.tsv ]; then
@@ -65,14 +71,14 @@ if [[ -z ${NUMBER_MGENES_FILES+x} || $NUMBER_MGENES_FILES -gt 0 ]]; then
       # ISL produces marker genes with the following fields:
       # clusts  padj    auroc   feat
       tail -n +2 $f | awk -F'\t' -v EXP_ID="$EXP_ID" -v k_value="$k" 'BEGIN { OFS = ","; }
-      { print EXP_ID, $4, k_value, $1, $2 }' >> $EXPERIMENT_MGENES_PATH/mgenesDataToLoad.csv
+      { print EXP_ID, $4, k_value, $1, $2 }' >> $markerGenesToLoad
     elif [ "$CLUSTERS_FORMAT" == "SCANPY" ]; then
       # Scanpy produces marker genes with the following fields with value like:
       # 
       # cluster	ref	rank	genes	scores	logfoldchanges	pvals	pvals_adj
       # 0	rest	0	FBgn0003448	20.580915	6.0675416	2.3605626738867564e-47	1.1128872726039114e-43
       tail -n +2 $f | awk -F'\t' -v EXP_ID="$EXP_ID" -v k_value="$k" 'BEGIN { OFS = ","; }
-      { print EXP_ID, $4, k_value, $1, $8 }' >> $EXPERIMENT_MGENES_PATH/mgenesDataToLoad.csv
+      { print EXP_ID, $4, k_value, $1, $8 }' >> $markerGenesToLoad
     else
       echo "ERROR: unrecognized CLUSTERS_FORMAT '"$CLUSTERS_FORMAT"', aborting load."
       echo "ERROR: this point should not have been reached..."
@@ -84,12 +90,10 @@ if [[ -z ${NUMBER_MGENES_FILES+x} || $NUMBER_MGENES_FILES -gt 0 ]]; then
   echo "Marker genes: Loading data for $EXP_ID..."
   
   set +e
-  printf "\copy scxa_marker_genes (experiment_accession, gene_id, k, cluster_id, marker_probability) FROM '%s' WITH (DELIMITER ',');" $EXPERIMENT_MGENES_PATH/mgenesDataToLoad.csv | \
+  printf "\copy scxa_marker_genes (experiment_accession, gene_id, k, cluster_id, marker_probability) FROM '%s' WITH (DELIMITER ',');" $markerGenesToLoad | \
     psql -v ON_ERROR_STOP=1 $dbConnection
 
   s=$?
-
-  rm $EXPERIMENT_MGENES_PATH/mgenesDataToLoad.csv
 
   # Roll back if write was unsucessful
   
@@ -101,4 +105,42 @@ if [[ -z ${NUMBER_MGENES_FILES+x} || $NUMBER_MGENES_FILES -gt 0 ]]; then
   fi
 
   echo "Marker genes: Loading done for $EXP_ID..."
+
+  # NEW LAYOUT: point at cell groups table, retrieving cell group integer IDs from there first 
+
+  # Get the group keys back from the auto-increment
+
+  echo "\copy (select concat(experiment_accession, '_', variable, '_', value), id from scxa_cell_group WHERE experiment_accession = '"$EXP_ID"' ORDER BY experiment_accession, variable, value) TO '$groupIds' CSV HEADER" | \
+    psql -v ON_ERROR_STOP=1 $dbConnection
+
+  # Get marker genes in the format 'expid_variable_value,cell_id,padj, where experiment, variable and value define the cell grouping
+  # First for cluster markers (with groups like k_1 etc)
+
+  cat markerGenesToLoad | awk -F',' '{print "\""$1"_k_"$3"_"$4"\",\""$2"\",\""$5"\""}' > $groupMarkerGenesToLoad.tmp
+  
+  # Add in the markers from annotation sources
+
+  if [ -e "$inferredCelltypeMarkers" ]; then
+    tail -n +2 $inferredCelltypeMarkers | awk -F'\t' -v EXP_ID="$EXP_ID" -'BEGIN { OFS = ","; } { print "\""EXP_ID"_inferred_cell_type_"$1"\"", "\""$4"\"", $8 }' >> ${groupMarkerGenesToLoad}.tmp
+  fi
+  if [ -e "$authorsInferredCelltypeMarkers" ]; then
+    tail -n +2 $authorsInferredCelltypeMarkers | awk -F'\t' -v EXP_ID="$EXP_ID" 'BEGIN { OFS = ","; } { print "\""EXP_ID"_inferred_cell_type_"$1"\"", "\""$4"\"", $8 }' >> ${groupMarkerGenesToLoad}.tmp  
+  fi
+
+  # Sort and join with the groups file to add the auto-incremented key from the groups table
+  cat ${groupMarkerGenesToLoad} | sort >> ${groupMarkerGenesToLoad}.tmp.sorted && rm -f ${groupMarkerGenesToLoad}.tmp
+  join -t , $groupIds ${groupMarkerGenesToLoad}.tmp.sorted | cut -d',' --complement -f1 > ${groupMarkerGenesToLoad}
+
+  nStartingMarkers=$(wc -l${groupMarkerGenesToLoad}.tmp.sorted | wc -l)
+  nFinalMarkers=$(wc -l ${groupMarkerGenesToLoad} | awk '{print $1}')
+
+  # Sanity check that the join worked
+
+  if [ ! "$nStartingMarkers" -eq "$nFinalMarkers" ]; then
+    echo "Final list of marker values ($nFinalMarkers) not equal to input number ($nStartingMarkers) after resolving keys to cell groups table." 1>&2
+    exit 1
+  fi
+
+  rm $markerGenesToLoad
+
 fi
