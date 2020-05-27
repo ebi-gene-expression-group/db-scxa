0911ce1a4c7207533764e55dd375e39b506be707
Jonathan Manning <jmanning@ebi.ac.uk>
2020-05-27 12:04:17 +0100

    Revert "Revert "Switch to data.table for file read/write""
    This reverts commit 9ec9e7557d2a6d3c81f19e1861cff9d8dac71bb0.

--- bin/wideSCCluster2longSCCluster.R
+++ bin/wideSCCluster2longSCCluster.R
@@ -2,6 +2,7 @@
 
 library(optparse)
 library(tidyr)
+library(data.table)
 
 option_list <- list( 
   make_option(c("-c", "--clusters-file"), dest="clusters_path"),
@@ -11,13 +12,14 @@ option_list <- list(
 
 opt <- parse_args(OptionParser(option_list=option_list))
 
-read.delim(opt$clusters_path, header = TRUE, check.names = FALSE, sep = '\t') -> clusters_wide
+fread(opt$clusters_path, header = TRUE, check.names = FALSE, sep = '\t') -> clusters_wide
 gather(clusters_wide, key = "cell_id", value = "cluster_id", -sel.K, -K) -> clusters_long
 
 clusters_long$experiment_accession <- opt$exp_acc
 names(clusters_long)[names(clusters_long) == 'K'] <- 'k'
 
-write.csv(clusters_long[,c('experiment_accession','cell_id','k','cluster_id')],
+fwrite(clusters_long[,c('experiment_accession','cell_id','k','cluster_id')],
           file=opt$output_path,
-          row.names = FALSE)
+          row.names = FALSE,
+          quote = TRUE)
 
