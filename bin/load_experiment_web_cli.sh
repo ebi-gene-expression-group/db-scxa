4c5d930da8152b4db91addd259ca55a3958a477b
Pablo Moreno <pablo.a.moreno@gmail.com>
2022-02-25 15:17:02 +0000

    New testing setup

---
+++ bin/load_experiment_web_cli.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+jar_dir=$CONDA_PREFIX/share/atlas-cli
+
+scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $scriptDir/common_routines.sh
+
+echo "CONDA_PREFIX: $CONDA_PREFIX"
+
+require_env_var "SOLR_HOST"
+require_env_var "ZK_HOST"
+require_env_var "ZK_PORT"
+require_env_var "BIOENTITIES"
+require_env_var "EXPERIMENT_FILES"
+require_env_var "jdbc_url"
+require_env_var "jdbc_username"
+require_env_var "jdbc_password"
+
+# Either ACCESSIONS or PRIVATE_ACCESSIONS need to be provided
+#require_env_var "ACCESSIONS"
+
+SOLR_PORT=$(get_port_from_hostport $SOLR_HOST)
+SOLR_HOST=$(get_host_from_hostport $SOLR_HOST)
+
+require_env_var "SOLR_PORT"
+
+java_opts="-Dsolr.host=$SOLR_HOST"
+java_opts="$java_opts -Dsolr.port=$SOLR_PORT"
+java_opts="$java_opts -Dzk.host=$ZK_HOST"
+java_opts="$java_opts -Dzk.port=$ZK_PORT"
+java_opts="$java_opts -Ddata.files.location=$BIOENTITIES"
+java_opts="$java_opts -Dexperiment.files.location=$EXPERIMENT_FILES"
+java_opts="$java_opts -Djdbc.url=$jdbc_url"
+java_opts="$java_opts -Djdbc.username=$jdbc_username"
+java_opts="$java_opts -Djdbc.password=$jdbc_password"
+java_opts="$java_opts -Djdbc.max_pool_size=2"
+java_opts="$java_opts -Dserver.port=8888"
+
+# Generate JSONL files from bulk experiments
+echo "PATH: "$PATH
+
+cmd="java $java_opts -jar $jar_dir/atlas-cli-sc.jar"
+cmd=$cmd" create-update-experiment"
+
+if [ ! -z ${failed_accessions_output+x} ]; then
+  cmd="$cmd -f $failed_accessions_output"
+fi
+
+if [ ! -z ${PRIVATE_ACCESSIONS+x} ]; then
+  cmd="$cmd -p $PRIVATE_ACCESSIONS"
+fi
+
+if [ ! -z ${ACCESSIONS+x} ]; then
+  cmd="$cmd -e $ACCESSIONS"
+fi
+echo "$cmd"
+$cmd
+
+status=$?
+
+exit $status
--- run_tests_with_containers.sh
+++ run_tests_with_containers.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-# Above ^^ will test that bash is installed
+
+export SOLR_HOST=my_solr:8983
+export ZK_HOST=gxa-zk-1
+export ZK_PORT=2181
+export POSTGRES_HOST=postgres
+export POSTGRES_DB=scxa-test
+export POSTGRES_USER=scxa
+export POSTGRES_PASSWORD=postgresPass
+export POSTGRES_PORT=5432
+export jdbc_url="jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
 
 docker_arch_line=""
 if [ $( arch ) == "arm64" ]; then
@@ -7,10 +16,50 @@ if [ $( arch ) == "arm64" ]; then
     echo "Changing arch $docker_arch_line"
 fi
 
-dbConnection='postgresql://scxa:postgresPass@postgres/scxa-test'
+dbConnection="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB"
 docker stop postgres && docker rm postgres
 docker network rm mynet
 docker network create mynet
-docker run --name postgres --net mynet -e POSTGRES_PASSWORD=postgresPass -e POSTGRES_USER=scxa -e POSTGRES_DB=scxa-test -p 5432:5432 -d postgres:10-alpine3.15
+
+docker run --rm --net mynet --name $ZK_HOST -d -p $ZK_PORT:$ZK_PORT -e ZOO_MY_ID=1 -e ZOO_SERVERS='server.1=0.0.0.0:2888:3888' -t zookeeper:3.4.14
+docker run --rm --net mynet --name my_solr -d -p 8983:8983 -e ZK_HOST=$ZK_HOST:$ZK_PORT -t solr:7.1-alpine -DzkRun -Denable.runtime.lib=true -m 2g
+
+
+docker run --rm --name postgres --net mynet \
+  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+  -e POSTGRES_USER=$POSTGRES_USER \
+  -e POSTGRES_DB=$POSTGRES_DB \
+  -p $POSTGRES_PORT:$POSTGRES_PORT -d postgres:10-alpine3.15
+
+sleep 20
+
+# migrate schemas to database
+docker run --rm -i --net mynet \
+  -v $( pwd )/atlas-schemas/flyway/scxa:/flyway/scxa \
+  quay.io/ebigxa/atlas-schemas-base:1.0 \
+  flyway migrate -url=$jdbc_url -user=$POSTGRES_USER \
+  -password=$POSTGRES_PASSWORD -locations=filesystem:/flyway/scxa
+
+# Test load of experiments through CLI
+docker run --rm -it --net mynet -v $( pwd )/tests:/usr/local/tests:rw \
+  -v $( pwd )/fixtures:/fixtures \
+  -v $( pwd )/bin:/usr/local/bin \
+  -e SOLR_HOST=$SOLR_HOST -e ZK_HOST=$ZK_HOST -e ZK_PORT=$ZK_PORT \
+  -e POSTGRES_USER=$POSTGRES_USER \
+  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+  -e jdbc_username=$POSTGRES_USER \
+  -e jdbc_password=$POSTGRES_PASSWORD \
+  -e jdbc_url=$jdbc_url \
+  -e ACCESSIONS=E-MTAB-2983 \
+  -e BIOENTITIES=/fixtures/ \
+  -e EXPERIMENT_FILES=/fixtures/experiment_files \
+  test/db-scxa-module load_experiment_web_cli.sh
+
 docker build $docker_arch_line -t test/db-scxa-module .
-docker run $docker_arch_line --net mynet -i -v $( pwd )/tests:/usr/local/tests:rw -e dbConnection=$dbConnection -v $( pwd )/atlas-schemas:/tmp/atlas-schemas:rw --entrypoint=/usr/local/tests/run_tests.sh test/db-scxa-module
+
+docker run --net mynet -i $docker_arch_line \
+  -v $( pwd )/tests:/opt/tests \
+  -v $( pwd )/atlas-schemas:/atlas-schemas \
+  -v $( pwd )/bin:/opt/bin \
+  -e dbConnection=$dbConnection \
+  --entrypoint=/opt/tests/run_tests.sh test/db-scxa-module
