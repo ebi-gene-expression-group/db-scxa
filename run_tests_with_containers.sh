a3ce8aab47688cde3c1ded86630e1de825a31069
Pablo Moreno <pablo.a.moreno@gmail.com>
2022-02-25 15:40:41 +0000

    Use base container and update atlas-schemas ref

--- run_tests_with_containers.sh
+++ run_tests_with_containers.sh
@@ -7,4 +7,4 @@ docker network rm mynet
 docker network create mynet
 docker run --name postgres --net mynet -e POSTGRES_PASSWORD=postgresPass -e POSTGRES_USER=scxa -e POSTGRES_DB=scxa-test -p 5432:5432 -d postgres:10.3-alpine
 docker build -t test/db-scxa-module .
-docker run --net mynet -i -v $( pwd )/tests:/usr/local/tests -e dbConnection=$dbConnection -v $( pwd )/atlas-schemas:/atlas-schemas --entrypoint=/usr/local/tests/run_tests.sh test/db-scxa-module
+docker run --net mynet -i -v $( pwd )/tests:/usr/local/tests -e dbConnection=$dbConnection -v $( pwd )/atlas-schemas:/tmp/atlas-schemas --entrypoint=/usr/local/tests/run_tests.sh test/db-scxa-module
