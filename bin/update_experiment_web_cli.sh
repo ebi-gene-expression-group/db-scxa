ea2c27a5eb8bed4d7bd196a58451f59ac04299c3
Pablo Moreno <pcm32@users.noreply.github.com>
2022-03-11 11:51:13 +0000

    Separate update experiment design through cli (#59)
    * Separate update experiment design with test.
    
    * Update explanations

--- bin/update_experiment_web_cli.sh
+++ bin/update_experiment_web_cli.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # 
 # This script:
-# - Checks if the experiment is loaded and stops it is already loaded.
-# - Adds the appropiate line to the experiments table if it doesn't exist.
-# - Generates the experiment design file from condensed SDRF and SDRF files in $EXPERIMENT_FILES/expdesign
+# - Checks if the experiment is loaded to do an update.
+# - Updates if needed the entry on the experiment table, keeping the original load date.
+# - Updates the experiment design file from condensed SDRF and SDRF files in $EXPERIMENT_FILES/expdesign
 #
 # Most of the variables required for this are usually defined in the environment file for each setup (test, prod, etc).
 # The experiment designs file might need to be synced to an appropiate location at the web application instance disk
@@ -24,9 +24,7 @@ require_env_var "EXPERIMENT_FILES"
 require_env_var "jdbc_url"
 require_env_var "jdbc_username"
 require_env_var "jdbc_password"
-
-# Either ACCESSIONS or PRIVATE_ACCESSIONS need to be provided
-#require_env_var "ACCESSIONS"
+require_env_var "ACCESSIONS"
 
 SOLR_PORT=$(get_port_from_hostport $SOLR_HOST)
 SOLR_HOST=$(get_host_from_hostport $SOLR_HOST)
@@ -53,19 +51,14 @@ java_opts="$java_opts -XX:UseAVX=0"
 echo "PATH: "$PATH
 
 cmd="java $java_opts -jar $jar_dir/atlas-cli-sc.jar"
-cmd=$cmd" create-update-experiment"
+cmd=$cmd" update-experiment-design"
 
 if [ ! -z ${failed_accessions_output+x} ]; then
   cmd="$cmd -f $failed_accessions_output"
 fi
 
-if [ ! -z ${PRIVATE_ACCESSIONS+x} ]; then
-  cmd="$cmd -p $PRIVATE_ACCESSIONS"
-fi
+cmd="$cmd -e $ACCESSIONS"
 
-if [ ! -z ${ACCESSIONS+x} ]; then
-  cmd="$cmd -e $ACCESSIONS"
-fi
 echo "$cmd"
 $cmd
 
