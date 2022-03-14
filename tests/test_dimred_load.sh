a1087b983012b154ed7e7c35b70f0a5f5310d1ec
Jonathan Manning <jmanning@ebi.ac.uk>
2022-03-14 20:57:55 +0000

    Fix quoting etc on param json, use script to delete

--- tests/test_dimred_load.sh
+++ tests/test_dimred_load.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# Delete table content for current EXP_ID
-echo "coords table: Delete rows for $EXP_ID:"
-echo "DELETE FROM scxa_coords WHERE experiment_accession = '"$EXP_ID"'" | \
-  psql -v ON_ERROR_STOP=1 $dbConnection
+delete_db_scxa_dimred.sh 
 
 # Normally we won't be extracting parameter values from file names, but we'll
 # do it just for testing
@@ -12,13 +9,14 @@ ls ${EXPERIMENT_DIMRED_PATH}/${EXP_ID}.tsne*.tsv | while read -r l; do
   export DIMRED_TYPE=tsne
   export DIMRED_FILE_PATH=$l
   paramval=$(echo "$l" | sed 's/.*[^0-9]\([0-9]*\).tsv/\1/g')
-  export DIMRED_PARAM_JSON="[{\"perplexity\": $paramval}]"
+  export DIMRED_PARAM_JSON='[{"perplexity": '$paramval'}]'
   load_db_scxa_dimred.sh
 done
+
 ls ${EXPERIMENT_DIMRED_PATH}/${EXP_ID}.umap*.tsv | while read -r l; do
   export DIMRED_TYPE=umap
   export DIMRED_FILE_PATH=$l
   paramval=$(echo "$l" | sed 's/.*[^0-9]\([0-9]*\).tsv/\1/g')
-  export DIMRED_PARAM_JSON="[{\"n_neighbors\": $paramval}]"
+  export DIMRED_PARAM_JSON='[{"n_neighbors": '$paramval'}]'
   load_db_scxa_dimred.sh
 done
