6ca13f65a9d30d5653ded0772db04da5d615aa41
Jonathan Manning <jmanning@ebi.ac.uk>
2022-03-14 12:38:55 +0000

    update tests

--- tests/test_dimred_load.sh
+++ tests/test_dimred_load.sh
@@ -8,12 +8,12 @@ ls ${EXPERIMENT_DIMRED_PATH}/${EXP_ID}.tsne*.tsv | while read -r l; do
   export DIMRED_FILE_PATH=$l
   paramval=$(echo "$l" | sed 's/.*[^0-9]\([0-9]*\).tsv/\1/g')
   export DIMRED_PARAM_JSON="[{\"perplexity\": $paramval}]"
-  run load_db_scxa_dimred.sh
+  load_db_scxa_dimred.sh
 done
 ls ${EXPERIMENT_DIMRED_PATH}/${EXP_ID}.umap*.tsv | while read -r l; do
   export DIMRED_TYPE=umap
   export DIMRED_FILE_PATH=$l
   paramval=$(echo "$l" | sed 's/.*[^0-9]\([0-9]*\).tsv/\1/g')
   export DIMRED_PARAM_JSON="[{\"n_neighbors\": $paramval}]"
-  run load_db_scxa_dimred.sh
+  load_db_scxa_dimred.sh
 done
