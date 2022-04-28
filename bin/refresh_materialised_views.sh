cea0da003357e4eba78c00eb0747530d0c76fc51
Jonathan Manning <jmanning@ebi.ac.uk>
2022-04-28 18:09:51 +0100

    Remove mat view stuff

--- bin/refresh_materialised_views.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-psql -v ON_ERROR_STOP=1 $dbConnection <<EOF
-SET maintenance_work_mem='2GB';
-REFRESH MATERIALIZED VIEW scxa_top_5_marker_genes_per_cluster;
-REFRESH MATERIALIZED VIEW scxa_marker_gene_stats;
-RESET maintenance_work_mem;
-EOF
