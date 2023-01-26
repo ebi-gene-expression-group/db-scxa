9427a9f7df00391e5665d0b62171649370c65f85
Haider Iqbal <haideri@ebi.ac.uk>
2023-01-26 17:00:31 +0000

    Handle factor value column descripencies

--- bin/load_exp_design.sh
+++ bin/load_exp_design.sh
@@ -14,6 +14,10 @@ sdrf_file=${SDRF_FILE:-$3}
 [ -z ${CONDENSED_SDRF_FILE+x} ] && echo "Env var CONDENSED_SDRF_FILE for the experiment design data needs to be defined." && exit 1
 [ -z ${SDRF_FILE+x} ] && echo "Env var SDRF_FILE for column sequence of experiment design needs to be defined." && exit 1
 
+# Reason for creating this array is to search factor value column
+# In some sdrf files this column is mentioned as "Factor Value" and in some as "FactorValue"
+ARRAY=( FactorValue "Factor Value" )
+
 while IFS=$'\t' read exp_acc sample sample_type col_name annot_value annot_url
 do
         if [ $sample_type == 'characteristic' ]
@@ -21,12 +25,14 @@ do
             search_column="Characteristics[${col_name}]"
             column_order=$(awk -v val="$search_column" -F '\t' '{for (i=1; i<=NF; i++) if ($i==val) {print i} }' $sdrf_file)
         else
-            search_column="FactorValue[${col_name}]"
-            column_order=$(awk -v val="$search_column" -F '\t' '{for (i=1; i<=NF; i++) if ($i==val) {print i} }' $sdrf_file)
+            for element in "${ARRAY[@]}"; do
+              search_column="$element[${col_name}]"
+              column_order=$(awk -v val="$search_column" -F '\t' '{for (i=1; i<=NF; i++) if ($i==val) {print i} }' $sdrf_file)
+            done
         fi
         echo "INSERT INTO exp_design_column (experiment_accession, column_name, sample_type, column_order) VALUES ('$exp_acc', '$col_name', '$sample_type', '$column_order');" | psql -v ON_ERROR_STOP=1 $dbConnection
         echo "INSERT INTO exp_design (sample, annot_value, annot_ont_uri, exp_design_column_id) VALUES ('$sample', '$annot_value', '$annot_url', (SELECT id FROM exp_design_column WHERE experiment_accession='$exp_acc' AND column_name='$col_name' AND sample_type='$sample_type'));" | psql -v ON_ERROR_STOP=1 $dbConnection
 
-done < $condensed_sdrf_file
+done
 
 echo "Experiment design data done loading!"
