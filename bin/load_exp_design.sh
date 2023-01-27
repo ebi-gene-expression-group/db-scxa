46351f386f57161f5896be2419c14250df733333
Haider Iqbal <haideri@ebi.ac.uk>
2023-01-27 14:35:36 +0000

    resolve duplicate constraint issue

--- bin/load_exp_design.sh
+++ bin/load_exp_design.sh
@@ -18,9 +18,11 @@ sdrf_file=${SDRF_FILE:-$3}
 # In some sdrf files this column is mentioned as "Factor Value" and in some as "FactorValue"
 FactorArray=( FactorValue "Factor Value" )
 
-while IFS=$'\t' read exp_acc sample sample_type col_name annot_value annot_url
+# for experiment design column table we need to have unique experiment accession, column name and sample type
+# as they are the primary key for the table and we don't want to insert duplicate rows
+cut -f 1,4,5 $condensed_sdrf_file | sort | uniq | while read exp_acc sample_type col_name;
 do
-        if [ $sample_type == 'characteristic' ]
+  if [ $sample_type == 'characteristic' ]
         then
             search_column="Characteristics[${col_name}]"
             column_order=$(awk -v val="$search_column" -F '\t' '{for (i=1; i<=NF; i++) if ($i==val) {print i} }' $sdrf_file)
@@ -34,8 +36,11 @@ do
             done
         fi
         echo "INSERT INTO exp_design_column (experiment_accession, column_name, sample_type, column_order) VALUES ('$exp_acc', '$col_name', '$sample_type', '$column_order');" | psql -v ON_ERROR_STOP=1 $dbConnection
-        echo "INSERT INTO exp_design (sample, annot_value, annot_ont_uri, exp_design_column_id) VALUES ('$sample', '$annot_value', '$annot_url', (SELECT id FROM exp_design_column WHERE experiment_accession='$exp_acc' AND column_name='$col_name' AND sample_type='$sample_type'));" | psql -v ON_ERROR_STOP=1 $dbConnection
+done
 
+while IFS=$'\t' read exp_acc sample sample_type col_name annot_value annot_url
+do
+  echo "INSERT INTO exp_design (sample, annot_value, annot_ont_uri, exp_design_column_id) VALUES ('$sample', '$annot_value', '$annot_url', (SELECT id FROM exp_design_column WHERE experiment_accession='$exp_acc' AND column_name='$col_name' AND sample_type='$sample_type'));" | psql -v ON_ERROR_STOP=1 $dbConnection
 done < $condensed_sdrf_file
 
 echo "Experiment design data done loading!"
