a71412ddbbad1d7721d658a277ee9257e3efb576
Iris Diana Yu <irisyu@ebi.ac.uk>
2024-06-14 13:29:39 +0100

    Remove unnecessary lines

--- bin/load_exp_design.sh
+++ bin/load_exp_design.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -e
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}" )" &> /dev/null && pwd )
-source "${SCRIPT_DIR}/common_routines.sh"
-
 scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]:-$0}" )" && pwd )
 source $scriptDir/common_routines.sh
 
